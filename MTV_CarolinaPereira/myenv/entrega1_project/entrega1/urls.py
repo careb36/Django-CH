0fc2e7ae935541191782b4f16e377d050f023c29
Carolina R. Pereira <58278841+careb36@users.noreply.github.com>
2022-07-28 23:20:37 -0300

    m

--- MTV_CarolinaPereira/myenv/entrega1_project/entrega1/urls.py
+++ MTV_CarolinaPereira/myenv/entrega1_project/entrega1/urls.py
@@ -4,7 +4,7 @@ from fmembers.views import list_fmembers, create_fmember
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('list_fmembers/', list_fmembers, name='list_fmembers'),
     path('create_fmember/', create_fmember, name='create_fmember'),
+    path('list_fmembers/', list_fmembers, name='list_fmembers'),
 ]
     
